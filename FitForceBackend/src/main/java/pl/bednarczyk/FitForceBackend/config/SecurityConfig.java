4971e08bbb6f97def696b906256afe551bbb60d9
Karol <114170012+karol199393@users.noreply.github.com>
2024-05-03 21:01:10 +0200

    Add Spring Security

--- FitForceBackend/src/main/java/pl/bednarczyk/FitForceBackend/config/SecurityConfig.java
+++ FitForceBackend/src/main/java/pl/bednarczyk/FitForceBackend/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package pl.bednarczyk.FitForceBackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,9 +16,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig  {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .dispatcherTypeMatchers(HttpMethod.valueOf("/api/**")).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic(httpBasicCustomizer());
+        return http.build();
+    }
+    private Customizer<HttpBasicConfigurer<HttpSecurity>> httpBasicCustomizer() {
+        return httpBasic -> httpBasic.realmName("Your Realm");
+    }
 
 
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -32,7 +49,6 @@ public class SecurityConfig {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 
     @Bean
     public UserDetailsService userDetailsService() {
